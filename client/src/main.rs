2da44cc0da0343e4a196846c2b0e7a83741fbb8f
mahmoud-eltahawy <gemmy9300@gmail.com>
2025-01-12 15:02:01 +0200

    events experiment

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,28 +2,37 @@ use std::{path::PathBuf, str::FromStr};
 
 use common::{Action, Unit, UnitKind};
 use iced::{
+    event,
+    keyboard::{self, Modifiers},
     widget::{column, image, row, Button, Row, Text},
-    Color, Element, Task,
+    Color, Element, Event, Subscription, Task,
 };
 
 mod action;
 
 fn main() -> iced::Result {
-    iced::application("eltahawy's locker", App::update, App::view).run()
+    iced::application("eltahawy's locker", App::update, App::view)
+        .subscription(App::subscription)
+        .run()
 }
 
 #[derive(Debug, Clone, Default)]
 struct App {
     units: Vec<Unit>,
+    shift: bool,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     Action(Action),
     Ls(Option<Vec<Unit>>),
+    EventOccurred(Event),
 }
 
 impl App {
+    fn subscription(&self) -> Subscription<Message> {
+        event::listen().map(Message::EventOccurred)
+    }
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::Action(action) => match action {
@@ -54,55 +63,93 @@ impl App {
                 }
                 Task::none()
             }
+            Message::EventOccurred(event) => {
+                match event {
+                    Event::Keyboard(event) => {
+                        println!("{:#?}", event);
+                        self.shift = event == keyboard::Event::ModifiersChanged(Modifiers::SHIFT);
+                    }
+                    Event::Mouse(event) => {
+                        println!("{:#?}", event);
+                    }
+                    Event::Window(event) => {
+                        println!("{:#?}", event);
+                    }
+                    Event::Touch(event) => {
+                        println!("{:#?}", event);
+                    }
+                }
+                Task::none()
+            }
         }
     }
 
     fn view(&self) -> Element<Message> {
         let ls = Button::new("ls home").on_press(Message::Action(Action::Ls(PathBuf::new())));
-        let rm = Button::new("rm /home/eltahawy/.bash_history").on_press(Message::Action(
-            Action::Rm(vec![Unit {
-                path: PathBuf::from_str("/home/eltahawy/.bash_profile").unwrap(),
+        let rm =
+            Button::new("rm .bash_history").on_press(Message::Action(Action::Rm(vec![Unit {
+                path: PathBuf::from_str(".bash_profile").unwrap(),
                 kind: common::UnitKind::File,
-            }]),
-        ));
-        let mv = Button::new("mv /home/eltahawy/.bash_history /home/eltahawy/Downloads").on_press(
-            Message::Action(Action::Mv {
-                from: vec![PathBuf::from_str("/home/eltahawy/.bash_history").unwrap()],
-                to: PathBuf::from_str("/home/eltahawy/Downloads").unwrap(),
-            }),
-        );
-        let cp = Button::new("cp /home/eltahawy/.bash_history /home/eltahawy/Downloads").on_press(
-            Message::Action(Action::Cp {
-                from: vec![PathBuf::from_str("/home/eltahawy/.bash_history").unwrap()],
-                to: PathBuf::from_str("/home/eltahawy/Downloads").unwrap(),
-            }),
-        );
-        let mp4 = Button::new("mp4 /home/eltahawy/record.mkv").on_press(Message::Action(
-            Action::Mp4(vec![PathBuf::from_str("/home/eltahawy/record.mkv").unwrap()]),
-        ));
+            }])));
+        let mv = Button::new("mv .bash_history Downloads").on_press(Message::Action(Action::Mv {
+            from: vec![PathBuf::from_str(".bash_history").unwrap()],
+            to: PathBuf::from_str("Downloads").unwrap(),
+        }));
+        let cp = Button::new("cp .bash_history Downloads").on_press(Message::Action(Action::Cp {
+            from: vec![PathBuf::from_str(".bash_history").unwrap()],
+            to: PathBuf::from_str("Downloads").unwrap(),
+        }));
+        let mp4 = Button::new("mp4 record.mkv").on_press(Message::Action(Action::Mp4(vec![
+            PathBuf::from_str("record.mkv").unwrap(),
+        ])));
+
+        let nav_bar = row![ls, rm, mv, cp, mp4].spacing(5).wrap();
 
         let units = self
             .units
             .iter()
-            .fold(Row::new().spacing(10), |acc, x| acc.push(unit_element(x)));
-        row![ls, rm, mv, cp, mp4, units.wrap()]
-            .spacing(5)
-            .wrap()
-            .into()
+            .map(UnitElement::new)
+            .fold(Row::new().spacing(10), |acc, x| acc.push(x.display()))
+            .wrap();
+
+        column![nav_bar, units].spacing(20).into()
     }
 }
 
-fn unit_element(unit: &Unit) -> Button<'static, Message> {
-    let path = match unit.kind {
-        UnitKind::Dirctory => "../public/directory.png",
-        UnitKind::Video => "../public/video.png",
-        UnitKind::Audio => "../public/audio.png",
-        UnitKind::File => "../public/file.png",
+struct UnitElement {
+    unit: Unit,
+    selected: bool,
+}
+
+macro_rules! dark_icon {
+    ($is_it:expr,$name:literal) => {
+        if $is_it {
+            concat!("../public/dark/", $name)
+        } else {
+            concat!("../public/", $name)
+        }
     };
-    let icon = Element::from(image(path).width(40).height(40)).explain(Color::BLACK);
-    let button = Button::new(column![icon, Text::new(unit.name())]).on_press_maybe(
-        matches!(unit.kind, UnitKind::Dirctory)
-            .then_some(Message::Action(Action::Ls(unit.path.clone()))),
-    );
-    button
+}
+
+impl UnitElement {
+    fn new(unit: &Unit) -> Self {
+        Self {
+            unit: unit.clone(),
+            selected: false,
+        }
+    }
+    fn display(&self) -> Button<'static, Message> {
+        let path = match self.unit.kind {
+            UnitKind::Dirctory => dark_icon!(self.selected, "directory.png"),
+            UnitKind::Video => dark_icon!(self.selected, "video.png"),
+            UnitKind::Audio => dark_icon!(self.selected, "audio.png"),
+            UnitKind::File => dark_icon!(self.selected, "file.png"),
+        };
+        let icon = Element::from(image(path).width(40).height(40)).explain(Color::BLACK);
+        let button = Button::new(column![icon, Text::new(self.unit.name())]).on_press_maybe(
+            matches!(self.unit.kind, UnitKind::Dirctory)
+                .then_some(Message::Action(Action::Ls(self.unit.path.clone()))),
+        );
+        button
+    }
 }
